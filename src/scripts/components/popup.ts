4a2e4e785f3f1634277ec5feff510af4c7654772
Vladimir Abdullaev <a.voxagamer@gmail.com>
2021-04-05 03:36:31 +0300

    Add new form for popup and update logic

--- src/scripts/components/popup.ts
+++ src/scripts/components/popup.ts
@@ -3,21 +3,37 @@ class Popup {
     readonly openElements: NodeListOf<HTMLButtonElement>;
     readonly closeElement: HTMLButtonElement;
 
+    readonly popupCallback: HTMLElement;
+    readonly popupOffer: HTMLElement;
+
     constructor() {
         this.wrapperElement = document.querySelector(`#wrapperPopup`) as HTMLElement;
         this.openElements = document.querySelectorAll(`.open-popup`) as NodeListOf<HTMLButtonElement>;
         this.closeElement = document.querySelector(`#closePopup`) as HTMLButtonElement;
 
-        this.openElements.forEach((oe) => oe.addEventListener(`click`, () => this.ShowPopup()));
+        this.popupCallback = document.querySelector(`#popupCallback`) as HTMLElement;
+        this.popupOffer = document.querySelector(`#popupOffer`) as HTMLElement;
+
+        this.openElements.forEach((oe) => oe.addEventListener(`click`, () => this.ShowPopup(oe)));
         this.closeElement.addEventListener(`click`, () => this.HidePopup());
     }
 
-    public ShowPopup(): void {
+    public ShowPopup(oe: HTMLButtonElement): void {
         this.wrapperElement.hidden = false;
+        switch (oe.dataset.popup) {
+            case `popupCallback`: {
+                this.popupCallback.hidden = false;
+                break;
+            }
+            case `popupOffer`:
+                this.popupOffer.hidden = false;
+        }
     }
 
     public HidePopup(): void {
         this.wrapperElement.hidden = true;
+        this.popupCallback.hidden = true;
+        this.popupOffer.hidden = true;
     }
 }
 
